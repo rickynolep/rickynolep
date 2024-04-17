75707b94509907aa0ec5292a3d2f4c2e7f5fd18e
RickyNolep <rickynolepcuy@gmail.com>
2024-04-17 20:58:55 +0800

    Styleeeeee

--- script.js
+++ script.js
@@ -5,4 +5,21 @@ document.addEventListener('DOMContentLoaded', function() {
             video.style.opacity = '0';
         }, 1200);
     });
+
+    var kemonomimiSpan = document.querySelector('.animated-underline');
+    var iconPlaceholder = document.querySelector('.icon-placeholder');
+    var roundedRectangle = document.querySelector('.rounded-rectangle');
+
+    kemonomimiSpan.addEventListener('click', function() {
+        // Fade out the icon placeholder
+        iconPlaceholder.style.animation = 'fadeOut 0.5s forwards';
+
+        // Resize the rounded rectangle
+        roundedRectangle.style.width = 'fit-content';
+        roundedRectangle.style.height = 'fit-content';
+
+        // Display the text
+        var popup = document.getElementById('popup');
+        popup.style.display = 'block';
+    });
 });
